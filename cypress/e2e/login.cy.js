f71b6ade79f0673198e3cc75c0dcab0583168a47
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-18 18:36:50 +0200

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,40 +1,27 @@
 const loginPage ='https://www.saucedemo.com/'
-var url = 'https://www.saucedemo.com/inventory.html'
- 
+var homePage = 'https://www.saucedemo.com/inventory.html'
+export {homePage}
 //function to check login fields
-export function login(username, password) {
-  cy.visit('https://www.saucedemo.com')
-    cy.get('[data-test="username"]').type(username)
-    cy.get('[data-test="password"]').type(password)
-    cy.get('[data-test="login-button"]').click()
-    cy.url().should('be.equal', url)
-}    
-
 describe('Swag Labs Login Test Scenarios', () => {
   //Correct username & correct Password
-  it('Positive Login Scenario', () => {
-    
-    login('performance_glitch_user', 'secret_sauce')
+ it('Positive Login Scenario', () => {
+    cy.login('performance_glitch_user','secret_sauce').url().should('be.equal', homePage)
   })
   //Correct username & incorrect Password
   it('Negative Login Scenario', () => {
-    url = loginPage
-    login('performance_glitch_user', '1234')
+    cy.login('performance_glitch_user', '1234')
   })
   //inCorrect username & correct Password
   it('Negative Login Scenario', () => {
-    url = loginPage
-    login('user', 'secret_sauce')
+    cy.login('user', 'secret_sauce')
   })
   //Not existing user
   it('Negative Login Scenario', () => {
-    url = loginPage
-    login('sarahkh', '@66SS')
+    cy.login('sarahkh', '@66SS')
   })
   //Empty fields
   it('Negative Login Scenario', () => {
-    url = loginPage
-    login(' ', ' ')
+    cy.login('\n', '\n')
   })
 
 })
