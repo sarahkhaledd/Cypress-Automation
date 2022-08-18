92b2f4f8791ae3705d9735c109204884db8b7d1e
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-18 20:41:27 +0200

    Update item-card.cy.js

--- cypress/e2e/item-card.cy.js
+++ cypress/e2e/item-card.cy.js
@@ -2,23 +2,17 @@ const cardPage = 'https://www.saucedemo.com/cart.html'
 
 describe('Swag Labs Adding&removing items to the Cart Test Scenarios', () => {
   //add a item to the card & the card will has a one item
-  it('Positive Add To Cart Scenario', () => {
+  it(' Add To Cart Scenario', () => {
+    cy.login('performance_glitch_user', 'secret_sauce')
     cy.addToCard().should('have.length', 1).url().should('be.equal',cardPage)
-  })
-  //add a item to the card & the card still empty , FAIL
-  it('Negative Add To Cart Scenario', () => {
-    cy.addToCard().should('have.length', 0).url().should('be.equal',cardPage)
   })
 
   /* --------------------------------------------------------------- */ 
 
   //add a item to the card & then remove it , card will be empty
-  it('Positive Remove To Cart Scenario', () => {
+  it(' Remove To Cart Scenario', () => {
+    cy.login('performance_glitch_user', 'secret_sauce')
     cy.removeFromCard().should('have.length', 0).url().should('be.equal',cardPage)
-  })
-  //add a item to the card & then remove it , still has a one item ,FAIL
-  it('Negative Remove To Cart Scenario', () => {
-    cy.removeFromCard().should('have.length', 1).url().should('be.equal',cardPage)
   })
 
 })
