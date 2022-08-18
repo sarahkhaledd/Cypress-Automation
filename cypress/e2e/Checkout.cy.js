9ff8f57a6f904838d0dd1cd0008ba72c20b92e2f
sarahkhaledd <61946261+sarahkhaledd@users.noreply.github.com>
2022-08-18 20:41:56 +0200

    Update Checkout.cy.js

--- cypress/e2e/Checkout.cy.js
+++ cypress/e2e/Checkout.cy.js
@@ -1,30 +1,37 @@
-import { login } from "./login.cy";
-import { addAndRemoveToCard } from "./item-card.cy";
-
-function checOut(name,lastName,postalCode)
+let checkoutPage = 'https://www.saucedemo.com/checkout-complete.html'
+, cartPage ='https://www.saucedemo.com/cart.html' ,
+infoPage = 'https://www.saucedemo.com/checkout-step-one.html'
+function checkOut(name,last,postalCode)
 {
   cy.get('[data-test="checkout"]').click()
   cy.get('[data-test="firstName"]').type(name)
-  cy.get('[data-test="lastName"]').type(lastName)
+  cy.get('[data-test="lastName"]').type(last)
   cy.get('[data-test="postalCode"]').type(postalCode)
   cy.get('[data-test="continue"]').click()
   cy.get('[data-test="finish"]').click()
-  cy.url().should('be.equal', 'https://www.saucedemo.com/checkout-complete.html')
+  cy.url().should('be.equal', checkoutPage)
 }
 
 describe('Swag Labs Login Test Scenarios', () => {
   //Correct Checkout Process 
   it('Positive Checkout Process Scenario', () => {
-    login('performance_glitch_user', 'secret_sauce')
-    addAndRemoveToCard(1)
-    checOut("sarah","khaled","8655")
+    cy.login('performance_glitch_user', 'secret_sauce')
+    cy.addToCard()
+    checkOut("sarah","khaled","8655")
   })
-  //empty information for Checkout Process 
+  //empty information fields for Checkout Process 
   it('Negative Checkout Process Scenario', () => {
-    login('performance_glitch_user', 'secret_sauce')
-    addAndRemoveToCard(1)
+    cy.login('performance_glitch_user', 'secret_sauce')
+    cy.addToCard()
     cy.get('[data-test="checkout"]').click()
     cy.get('[data-test="continue"]').click()
-  
+    cy.url().should('be.equal', infoPage)
+  })
+  //empty items then Checkout
+  it('Negative Checkout Process Scenario', () => {
+    cy.login('performance_glitch_user', 'secret_sauce') 
+    cy.get('.shopping_cart_link').click()
+    cy.get('[data-test="checkout"]').click().url().should('be.equal', cartPage)
+
   })
 })
